33d1c461c0455ef92bce3306978cec318ecd0f03
Evon Bausa <63164022+ebauss@users.noreply.github.com>
2022-03-13 10:27:57 -0700

    tapout on the chat prototype for now

--- scripts/chat-prototype.js
+++ scripts/chat-prototype.js
@@ -19,36 +19,40 @@ function postMessage(message) {
     })
 }
 
-// need to fix this code. Doesn't work yet.
+// Loads chat messages history and listens for upcoming ones.
 function loadMessages() {
-    // Create the query to load the last 12 messages and listen for new ones.
-    const recentMessagesQuery = query(collection(getFirestore(), 'chattest'), limit(12));
-    
-    // Start listening to the query.
-    onSnapshot(recentMessagesQuery, function(snapshot) {
-      snapshot.docChanges().forEach(function(change){
-        var message = change.doc.data();
+    // listen for incoming messages
+    firebase.database().ref("messages").on("child_added", function (snapshot) {
+        var html = "";
+        // show delete button if message is sent by me
+        if (snapshot.val().sender == myName) {
+            console.log("new message")
+        }
+        // html += snapshot.val().sender + ": " + snapshot.val().message;
+        // html += "</li>";
 
-        styled_result = '<div class="message message-out">' + '<a href="#" data-bs-toggle="modal" data-bs-target="#modal-profile" class="avatar avatar-responsive">' + '<img class="avatar-img" src="assets/img/avatars/1.jpg" alt="">' + '</a>' + '<div class="message-inner">' + '<div class="message-body">' + '<div class="message-content">' + '<div class="message-text">' + '<p class="firebase_stored">' + message + "something erro?" + '</p>' + '</div>' + '</div>' + '</div>' + '</div>' + '</div>'
-        old_div_content = jQuery('#chatmain').html();
-        new_div_content = old_div_content + styled_result;
-        jQuery('#chatmain').html(new_div_content);})} )}
+        // document.getElementById("messages").innerHTML += html;
+    });
+}
 
 
 function sendMessage() {
     var messageText = $("#message-text").val();
     console.log(`messageText = ${messageText}`);
     oldMessages = $("#text-container").html();
-    
-    newMessage = `<div><div class="alert alert-primary" role="alert" style="float: right; clear: both">${messageText}</div></div>`
+
+    newMessage = `<li><div class="alert alert-primary" role="alert" style="float: right; clear: both">${messageText}</div></ls>`
 
     $("#text-container").html(oldMessages + newMessage)
+
+    // return oldMessages + newMessage
 
     // post the message content to messages collection in firebase
     postMessage(messageText);
 }
 
 function setup() {
+    loadMessages();
     $("#send-button").click(sendMessage);
 }
 

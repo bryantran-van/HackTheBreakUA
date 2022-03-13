d9ce0a20267d2696817b88efdae0739a905f780d
nng32 <nng32@my.bcit.ca>
2022-03-12 20:19:16 -0800

    Modify main.html to change requests link depending on whether user is a volunteer

--- scripts/main.js
+++ scripts/main.js
@@ -15,3 +15,34 @@ function insertName(){
     })
 }
 insertName();
+
+function changeRequestsLink() {
+    // check if user is signed in
+    firebase.auth().onAuthStateChanged(user => {
+        if (user) {
+            console.log("Retrieving data from " + user.uid);
+
+            db.collection("users").doc(user.uid).get().then(userDoc => {
+                role = userDoc.data().role;
+
+                if (role == "Volunteer") {
+                    console.log("Loaded volunteer request link");
+                    $('#requests-title').html("View help requests");
+                    $('#requests-desc').html("View the backlog of help requests by civilians and fighters.");
+                    $('#requests-link').attr("href", "../help-form/requests.html");
+                }
+                else {
+                    console.log("Loaded civilian/fighter request link");
+                    $('#requests-title').html("Submit help request");
+                    $('#requests-desc').html("Post a request for help. Friendly volunteers will be able to view and accept your requests.");
+                    $('#requests-link').attr("href", "../help-form/help-form.html");
+                }
+            })
+        }
+        else {
+            alert("Failed to retrieve data. Please check to make sure you are signed in.");
+        }
+    })
+}
+
+changeRequestsLink();

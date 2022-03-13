6e1513677ae09f217472480bda5c5253500445db
Evon Bausa <63164022+ebauss@users.noreply.github.com>
2022-03-13 03:09:27 -0700

    add loadMessages() that doesn't work yet.

--- scripts/chat-prototype.js
+++ scripts/chat-prototype.js
@@ -17,9 +17,22 @@ function postMessage(message) {
             alert("Failed to retrieve data. Please check to make sure you are signed in.");
         }
     })
-
-
 }
+
+// need to fix this code. Doesn't work yet.
+function loadMessages() {
+    // Create the query to load the last 12 messages and listen for new ones.
+    const recentMessagesQuery = query(collection(getFirestore(), 'chattest'), limit(12));
+    
+    // Start listening to the query.
+    onSnapshot(recentMessagesQuery, function(snapshot) {
+      snapshot.docChanges().forEach(function(change){
+        var message = change.doc.data();
+
+        styled_result = '<div class="message message-out">' + '<a href="#" data-bs-toggle="modal" data-bs-target="#modal-profile" class="avatar avatar-responsive">' + '<img class="avatar-img" src="assets/img/avatars/1.jpg" alt="">' + '</a>' + '<div class="message-inner">' + '<div class="message-body">' + '<div class="message-content">' + '<div class="message-text">' + '<p class="firebase_stored">' + message + "something erro?" + '</p>' + '</div>' + '</div>' + '</div>' + '</div>' + '</div>'
+        old_div_content = jQuery('#chatmain').html();
+        new_div_content = old_div_content + styled_result;
+        jQuery('#chatmain').html(new_div_content);})} )}
 
 
 function sendMessage() {
